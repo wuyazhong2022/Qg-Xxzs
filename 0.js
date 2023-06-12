209e9ae0e8c97097d5f30f3077525d42128e1032
wuyazhong2022 <101036230+wuyazhong2022@users.noreply.github.com>
2023-06-12 22:03:05 +0800

    Update 0.js

--- 0.js
+++ 0.js
@@ -123,7 +123,7 @@ var storage = storages.create('songgedodo');
 // 脚本版本号
 var last_version = "V12.0";
 var engine_version = "V12.3";
-var newest_version = "V12.4";
+var newest_version = "V12.5";
 if (storage.get(engine_version, true)) {
     storage.remove(last_version);
         let gengxin_rows = "脚本有风险，仅供学习交流;更新内容：;1.原脚本会进入“我的”界面获取用户名，区分历史刷过文章，现取消此设定;2.可自定义滑动验证界面震动提醒时间;3.禁止截屏会随机选一个选项;4.自定义评论内容;5.趣味答题有时间再更新修复；脚本测试环境：强国V2.46.0；（点击取消不再提示）".split(";");
