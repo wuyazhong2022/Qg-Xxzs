0409e9e5ee06adc6c7dc5a3bc50eb105b680b953
wuyazhong2022 <101036230+wuyazhong2022@users.noreply.github.com>
2023-06-13 09:20:44 +0800

    Update UI.js

--- UI.js
+++ UI.js
@@ -304,7 +304,7 @@ var apkurl = "https://github.com/wuyazhong2022/Qg-Xxzs/releases/download/v2.2.4/
 var latest_version = "2.2.4";
 if (GLOBAL_CONFIG.get("NO_UPDATE", 0) && (app.versionName != latest_version)) {
     ui.update.visibility = 0;
-    ui.update.setText("点击更新至最新版v" + latest_version);
+    ui.update.setText("有更新点击更新至最新版v" + latest_version);
 } else if (app.versionName != latest_version) {
     checkversion();
 }
