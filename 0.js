7a8a6e81a551f5b95e4605fffd8966fea6196b9c
wuyazhong2022 <101036230+wuyazhong2022@users.noreply.github.com>
2023-10-27 12:14:47 +0800

    Update 0.js

--- 0.js
+++ 0.js
@@ -122,7 +122,7 @@ sleep(delay_time);
 var storage = storages.create('songgedodo');
 // 脚本版本号
 var last_version = "V12.0";
-var engine_version = "V12.6";
+var engine_version = "V12.7";
 var newest_version = "V12.7";
 if (storage.get(engine_version, true)) {
     storage.remove(last_version);
